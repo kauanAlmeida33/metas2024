6c2467485a346cd731e091bc2405149fee79d36c
kauanAlmeida33 <105437759+kauanAlmeida33@users.noreply.github.com>
2024-04-12 08:29:58 -0300

    j

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ const tempoObjetivo4 = new Date("2024-10-05T00:00:00");
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 function calculaTempo(tempoObjetivo){
 
-function calculaTempo(tempoObjetivo) 
+function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
     let segundos = Math.floor(tempoFinal / 1000);
