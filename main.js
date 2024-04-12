7b6c80538d3a2e9e419059eb7c917bea8a4fc46c
kauanAlmeida33 <105437759+kauanAlmeida33@users.noreply.github.com>
2024-04-12 08:33:45 -0300

    cx

--- main.js
+++ main.js
@@ -14,7 +14,6 @@ for (let i = 0; i < botoes.length; i++) {
     }
 }
 
-
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
 const tempoObjetivo2 = new Date("2024-10-05T00:00:00");
@@ -22,6 +21,9 @@ const tempoObjetivo3 = new Date("2024-10-05T00:00:00");
 const tempoObjetivo4 = new Date("2024-10-05T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
+contadores[0].textContent = calculaTempo[tempoObjetivo1]
+
 function calculaTempo(tempoObjetivo){
 
 function calculaTempo(tempoObjetivo) {
