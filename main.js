adedf564fef51af748c15c3d081ee4ddc7178167
kauanAlmeida33 <105437759+kauanAlmeida33@users.noreply.github.com>
2024-04-19 08:28:08 -0300

    sdsd

--- main.js
+++ main.js
@@ -13,30 +13,40 @@ for (let i = 0; i < botoes.length; i++) {
         textos[i].classList.add("ativo");
     }
 }
-
+//cria as constantes e a lista "tempo" dos objetivos ou metas
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2024-10-05T00:00:00");
-const tempoObjetivo3 = new Date("2024-10-05T00:00:00");
-const tempoObjetivo4 = new Date("2024-10-05T00:00:00");
-
+const tempoObjetivo1 = new Date("2024-02-02T00:00:00");
+const tempoObjetivo2 = new Date("2024-12-15T00:00:00");
+const tempoObjetivo3 = new Date("2024-11-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-06-30T00:00:00");
+//constante da lista dos tempos de objetivo
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
-contadores[0].textContent = calculaTempo[tempoObjetivo1]
-
-function calculaTempo(tempoObjetivo){
-
 function calculaTempo(tempoObjetivo) {
-    let tempoAtual = new Date();
-    let tempoFinal = tempoObjetivo - tempoAtual;
-    let segundos = Math.floor(tempoFinal / 1000);
-    let minutos = Math.floor(segundos / 60);
-    let horas = Math.floor(minutos / 60);
-    let dias = Math.floor(horas / 24);
+    let tempoAtual = new Date(); //obtem a data atual
+    let tempoFinal = tempoObjetivo - tempoAtual; //calcula o tempo final restante
+    let segundos = Math.floor(tempoFinal / 1000); //calcula os segundos restantes
+    let minutos = Math.floor(segundos / 60);//calcula os minutos restantes
+    let horas = Math.floor(minutos / 60);//calcula as horas restantes
+    let dias = Math.floor(horas / 24);//calcula os dias restantes
    
-    segundos %= 60;
-    minutos %= 60;
-    horas %= 24;
+    segundos %= 60;  //obtem o resto da divisão dos segundos
+    minutos %= 60;   //obtem o resto da divisão dos minutos
+    horas %= 24;    //obtem o resto da divisão das horas
 
-        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    if (tempoFinal > 0){
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos " 
+    }else{
+        return " PRAZO ENCERRADO!!! "
+    }
 }
+function atualizaCronometro(){
+    for (let i = 0; < contadores.length; i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    }
+}
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro, 1000);
+}
+comecaCronometro();
