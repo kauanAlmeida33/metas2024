c3339d940f07cf9d7bb694c0b0bcab3545706a29
kauanAlmeida33 <105437759+kauanAlmeida33@users.noreply.github.com>
2024-04-26 08:30:27 -0300

    fff

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 //cria as constantes e a lista "tempo" dos objetivos ou metas
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-02-02T00:00:00");
-const tempoObjetivo2 = new Date("2024-12-15T00:00:00");
+const tempoObjetivo1 = new Date("2024-08-17T00:00:00");
+const tempoObjetivo2 = new Date("2024-12-25T00:00:00");
 const tempoObjetivo3 = new Date("2024-11-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-06-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-10-01T00:00:00");
 //constante da lista dos tempos de objetivo
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
